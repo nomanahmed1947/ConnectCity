40963b1199ade5cc3dfa828a91b2f008bf1d9f98
Noman Ahmed <nomanahmed1947@gmail.com>
2020-08-07 12:02:09 -0400

    done

--- src/main/java/com/na/handler/ConnectCityExceptionHandler.java
+++ src/main/java/com/na/handler/ConnectCityExceptionHandler.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ConnectCityExceptionHandler {
 	
+	@ExceptionHandler
+	public ResponseEntity<String> handleInvalidOriginDestination(InvalidParameterException exception) {
+		return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+	
 	@ExceptionHandler(MissingServletRequestParameterException.class)
 	public ResponseEntity<String> handleMissingParams(MissingServletRequestParameterException exception) {
 		return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
